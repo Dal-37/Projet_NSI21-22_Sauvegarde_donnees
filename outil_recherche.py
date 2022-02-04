780dc51d2c1d68222bf158736107de54b3d9c049
Dal-37 <69385767+Dal-37@users.noreply.github.com>
2022-02-04 09:55:26 +0100

    Création de la lecture de ligne de commande

---
+++ outil_recherche.py
@@ -0,0 +1,36 @@
+import os
+
+#Outil de recherche
+
+#commande de l'utilisateur
+#commande = input(">>>")
+
+#on decoupe la requete de l'utilisateur et on cherche à quelle fonction
+#elle correspond
+#reqst = commande.split(" ")
+name = ""
+size = ""
+led = ""
+ext = ""
+
+if reqst[0] == "find":
+    #parametre de la fonction find:
+        #-name
+        #-size
+        #-led (last edit date)
+        #-ext
+    # en fonction de la longueur de la liste on cherche les parametres
+    for i in range(2, len(reqst)):
+        #on cherche tout les parametres
+        #nom
+        if "-name" in reqst[i]:
+            name = reqst[i].split("=")[1]
+        #taille
+        elif "-size" in reqst[i]:
+            size = reqst[i].split("=")[1]
+        #date de modification
+        elif "-date" in reqst[i]:
+            led = reqst[i].split("=")[1]
+        #extension 
+        elif "-ext" in reqst[i]:
+            ext = reqst[i].split("=")[1]
