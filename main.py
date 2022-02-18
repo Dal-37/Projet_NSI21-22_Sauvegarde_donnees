2565732f5fdf5eebf13f7c740044f8c4c2e11273
Dal-37 <69385767+Dal-37@users.noreply.github.com>
2022-02-18 10:15:05 +0100

    Ajout de l'outil de planification

--- main.py
+++ main.py
@@ -146,3 +146,38 @@ elif reqst[0] == "copy":
             
             
     shutil.copyfile(source,destination)
+    
+elif reqst[0] == "plan":
+    chemins = input("Rentrez le chemin vers le fichier à copier et celui vers le dossier de sauvegarde")
+    saved_file = chemins.split(" ")[0]
+    save_dos = chemins.split(" ")[1]
+ 
+    
+    frequence = input("""Choisissez la fréquence de sauvegarde:\n
+                      10min: -dix; 30min: -trente; 1h: -une; 6h: -quart; 12h: -demi; 24h: -jour
+                      """)
+                      
+    delay = 0.0
+    if frequence == "-dix":
+        delay = 10.0*60.0
+    elif frequence == "-trente":
+        delay = 30.0*60.0
+    elif frequence == "-une":
+        delay = 3600.0
+    elif frequence == "-quart":
+        delay = 6.0*3600.0
+    elif frequence == "demi":
+        delay = 12.0*3600.0
+    elif frequence == "-jour":
+        delay = 24.0*3600.0
+    
+    #fonction à appeler par le module threading
+    def copie_plan():
+        global saved_file, save_dos
+        shutil.copyfile(saved_file, save_dos)
+        print("Copie effectuée")
+        
+        time.sleep(delay)
+        copie_plan()
+        
+    copie_plan()
